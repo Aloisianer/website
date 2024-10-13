5b68f8f343ed00a2a69e3f66a1aadb20e4ff15b9
Babygrill ☕️ <139216162+FerrousInk@users.noreply.github.com>
2024-10-13 21:39:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("🇩🇪 I use Arch btw~")
+
+document.getElementById("toggle-dark-white").addEventListener("click", function() {
+    const html = document.getElementById("host-dark-white");
+    const button = document.getElementById("toggle-dark-white");
+    const htmlClass = html.className;
+
+    if (htmlClass.includes("dark-mode")) {
+        html.className = htmlClass.replace("dark-mode", "white-mode")
+        button.textContent = button.textContent.replace("•", "●")
+    } else {
+        html.className = htmlClass.replace("white-mode", "dark-mode")
+        button.textContent = button.textContent.replace("●", "•")
+    }
+});
