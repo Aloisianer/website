72a9620c94ac6746dbcb2f2640dd9b66a49e52ab
Alois~ <139216162+FerrousInk@users.noreply.github.com>
2025-02-02 18:08:18 +0000

    Updated Some Stuff

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("🇩🇪 I use Arch btw~")
+console.log("I use Arch btw~")
 
 document.getElementById("toggle-dark-white").addEventListener("click", function() {
     const html = document.getElementById("host-dark-white");
