4f6edbee7ab2d6833a7a68cec332220d5972f9c5
Alois~ <moaugustin@proton.me>
2025-04-28 15:44:32 +0200

    Centered Stuff

---
+++ _next/static/chunks/app/layout-b2446a74e5b0dd18.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1633:()=>{},3851:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,1633,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[424,364,770,358],()=>s(3851)),_N_E=e.O()}]);
